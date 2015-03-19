d71c5ec67f01d35ae50d8b6f7025183a16e1b8eb
Vinaya kasirala <vinaya_kasirala@itss.vic.gov.au>
2015-03-19 16:33:52 +1100

    Added validations to the coordinates in table class

--- lib/helpers/table.rb
+++ lib/helpers/table.rb
@@ -1,16 +1,24 @@
 =begin
 Class for Table
 This class also contains method to return if robot is placed on the table
+This class also verifies if the coordinates are valid based on the table dimensions
 =end
 
 class Table
 
   attr_accessor :position
   def place(x, y)
-    self.position = { x: x, y: y }
+    self.position = { x: x, y: y }  if valid_coordinates?(x, y)
   end
 
   def is_placed?
     self.position != nil
   end
+    
+private
+
+  def valid_coordinates?(x, y)
+    (x >= 0 && x <= 4 && y >= 0 && y <= 4)
+  end
+    
 end
